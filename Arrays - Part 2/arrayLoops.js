82788a98a3c7d5f2e240f389d0d8566656143fcf
MaishaTahsin <rubaitreshad@gmail.com>
2025-05-20 04:59:55 +0600

    lecture notes

---
+++ Arrays - Part 2/arrayLoops.js
@@ -0,0 +1,14 @@
+// Loops:
+// 1. for loops
+// 2. while loops
+// 3. do while loops -->ignore
+// 4. for of loops --> array 
+// 5. for in loops --> object
+
+const friends = ['Wade', 'Ayaz', 'Akhund'];
+
+for (const friend of friends){
+    console.log(friend);
+}
+
+// other loops are similar to other languages
---
+++ Arrays - Part 2/reverseSortArrayLoops.js
@@ -0,0 +1,32 @@
+// const numbers = [1,2,3,4,5,6,7];
+
+// const reverse = numbers.reverse();
+
+//manually
+
+// const rev_numbers = []
+
+// for (const num of numbers){
+//     // console.log(num)
+//     rev_numbers.unshift(num)
+// }
+
+// console.log(rev_numbers)
+
+// more manually
+
+// for (let i = numbers.length -1 ; i>=0; i-- )
+// {
+//     const num = numbers[i];
+//     console.log(num);
+// }
+
+// SORT
+
+// const numbers = [4,7,12,8,43,6,1];
+// asc
+// const numbers_asc = [...numbers].sort(function(a,b) {return a - b})
+// dsc
+// const numbers_dsc = [...numbers].sort(function(a,b) {return b - a})
+// console.log(numbers_asc);
+// console.log(numbers_dsc);
